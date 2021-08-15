0f101401ee49523da75b47352a0fef4cebf01872
Egor Fedorenko <fedore.egor@gmail.com>
2021-08-15 18:09:44 +0300

    idk if it works im just saving it

--- src/main/java/photobooth/MainServlet.java
+++ src/main/java/photobooth/MainServlet.java
@@ -1,6 +1,12 @@
 package photobooth;
 
+import database.DBFactory;
+import database.DBInterface;
+import lombok.SneakyThrows;
+
 import java.io.*;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,19 +15,25 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/photobooth")
 public class MainServlet extends HttpServlet {
+    private static DBInterface db;
 
     @Override
+    public void init() throws ServletException {
+        db = DBFactory.getInstance();
+    }
+
+    @Override
+    @SneakyThrows //uwu sneaky hehe
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         var param = req.getParameter("first");
-        var clientCode = param==null? "" : param.replaceAll("<","&lt;").replaceAll(">","&gt;");//ваиант перекрытия атаки xss
+        var clientCode = param == null ? "" : param.replaceAll("<", "&lt;").replaceAll(">", "&gt;");//ваиант перекрытия атаки xss (посоветовали умные дяди ашто?)
         System.out.println(clientCode);
+        var code = Integer.parseInt(clientCode);
         try {
-            // TODO: 10.08.2021 - получаем код - проверяем его в базе данных - получаем соотв. путь к фотографиям - архивируем - отправляем zip
-            // TODO: 10.08.2021 - написать код для добавления фотографий в систему (добавление в БД соответствия пароль-путь, добавление фото по заданному пути)
-            var zip = ZipArchive.makeZIP(clientCode);
+            var path = PostgresPhotoPath.getPath(db, code);
+            var zip = ZipArchive.makeZIP(path);
             resp.getOutputStream().write(zip);
-        }
-        catch (Exception e){
+        } catch (SQLException e) {
             resp.sendRedirect("/error.html");
         }
     }
--- src/main/java/photobooth/PostgresPhotoPath.java
+++ src/main/java/photobooth/PostgresPhotoPath.java
@@ -1,7 +1,14 @@
 package photobooth;
 
-public class PostgresPhotoPath {
-    public static void getPath(String userCode){
+import database.DBInterface;
 
+import java.sql.SQLException;
+
+public class PostgresPhotoPath {
+    public static String getPath(DBInterface db, int userCode) throws SQLException {
+        String path = "";
+        var isAuth = db.authenticate(userCode);
+        if (isAuth) return db.loadPath(userCode);
+        else throw new SQLException("Code is not correct");
     }
 }
